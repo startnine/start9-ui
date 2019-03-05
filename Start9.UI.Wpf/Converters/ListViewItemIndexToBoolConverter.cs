99894a05b7d24cf132593deb9842658a50823637
Splitwirez <Splitwirez@gmail.com>
2019-03-05 12:18:22 -0500

    Added `BreadcrumbsBar`

--- Start9.UI.Wpf/Converters/ListViewItemIndexToBoolConverter.cs
+++ Start9.UI.Wpf/Converters/ListViewItemIndexToBoolConverter.cs
@@ -15,27 +15,32 @@ namespace Start9.UI.Wpf.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            FrameworkElement item = values[0] as FrameworkElement;
-            Panel panel = values[1] as Panel;
-            string position = (string)parameter;// (values[2]);
-
-            bool returnValue = false;
-
-            if (panel != null)
+            if ((values[0] != null) && (values[1] != null))
             {
-                int index = panel.Children.IndexOf(item);
+                FrameworkElement item = values[0] as FrameworkElement;
+                Panel panel = values[1] as Panel;
+                string position = (string)parameter;// (values[2]);
 
-                if (position.ToLowerInvariant() == "first")
-                    returnValue = index == 0;
-                else if (position.ToLowerInvariant() == "last")
-                    returnValue = index == (panel.Children.Count - 1);
-                else if (position.ToLowerInvariant() == "only")
-                    returnValue = (panel.Children.Count == 1) && (index == 0);
+                bool returnValue = false;
+
+                if (panel != null)
+                {
+                    int index = panel.Children.IndexOf(item);
+
+                    if (position.ToLowerInvariant() == "first")
+                        returnValue = index == 0;
+                    else if (position.ToLowerInvariant() == "last")
+                        returnValue = index == (panel.Children.Count - 1);
+                    else if (position.ToLowerInvariant() == "only")
+                        returnValue = (panel.Children.Count == 1) && (index == 0);
+                }
+
+                //Debug.WriteLine("conditions: " + (position.ToLowerInvariant() == "first").ToString() + ", " + (position.ToLowerInvariant() == "last").ToString() + ", " + (position.ToLowerInvariant() == "only").ToString() + "; values: " + (item != null).ToString() + ", " + (panel != null).ToString() + "; parameter: " + position.ToLowerInvariant() + "; returnValue: " + returnValue.ToString());
+                //conditions: False, True; values: True, True; parameter:last; returnValue: False
+                return returnValue;
             }
-
-            //Debug.WriteLine("conditions: " + (position.ToLowerInvariant() == "first").ToString() + ", " + (position.ToLowerInvariant() == "last").ToString() + ", " + (position.ToLowerInvariant() == "only").ToString() + "; values: " + (item != null).ToString() + ", " + (panel != null).ToString() + "; parameter: " + position.ToLowerInvariant() + "; returnValue: " + returnValue.ToString());
-            //conditions: False, True; values: True, True; parameter:last; returnValue: False
-            return returnValue;
+            else
+                return false;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
